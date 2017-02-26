4a508eb4aa3572721d14b7c79c7e8bc79673cda5
Pengcheng <pengchengcao@hotmail.com>
2017-02-26 21:05:23 +1100

    the little poc is working!

--- buildScripts/server.js
+++ buildScripts/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var open = require('open');
+var fs = require('fs');
 
 
 var port = 3000;
@@ -8,9 +9,12 @@ var port = 3000;
 var app = express();
 
 //set view engine
-app.set('views','./src/ejs');
+app.set('views', './src');
 app.set('view engine', 'ejs');
 
+
+//static files
+app.use('/static', express.static('./src/component'));
 
 //route
 app.get('/', function (req, res) {
@@ -18,13 +22,34 @@ app.get('/', function (req, res) {
 });
 
 //render pages
-app.get('/preview/:templateName',function(req,res){
+app.get('/preview/:templateName', function (req, res) {
 
 	//get template name first
 	var tempName = req.params.templateName.toString();
-	res.render(tempName,{});
+	res.render('ejs/' + tempName, {});
 
 });
+
+app.get('/previewComponent/:templateName/:componentName', function (req, res) {
+
+	//get template and component name
+	var tempName = req.params.templateName.toString();
+	var componentName = req.params.componentName.toString();
+	var componentPath = '../src/component/' + componentName + '/' + componentName + '.ejs';
+	var componentJsonPath = '../src/component/' + componentName + '/' + componentName + '.json';
+
+
+	//read assets from json file passed in
+	var jsonPath = path.join(__dirname, componentJsonPath);
+	var assets = JSON.parse(fs.readFileSync(jsonPath, 'utf8'));
+
+	res.render('template/' + tempName, {
+		componentPath: componentPath,
+		assets: assets
+	});
+
+});
+
 
 app.listen(port, function (err) {
 
---
+++ src/component/polling/js/polling.js
@@ -0,0 +1,1 @@
+console.log('from polling1');
