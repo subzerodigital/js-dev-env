eb667a1772ba0068dc09cea83d7c6a61d02cf7a3
Pengcheng <pengchengcao@hotmail.com>
2017-02-26 14:12:06 +1100

    express integration with browserSync

--- buildScripts/server.js
+++ buildScripts/server.js
@@ -7,9 +7,22 @@ var port = 3000;
 
 var app = express();
 
-app.get('/', function (req, res) {
+//set view engine
+app.set('views','./src/ejs');
+app.set('view engine', 'ejs');
 
+
+//route
+app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+//render pages
+app.get('/preview/:templateName',function(req,res){
+
+	//get template name first
+	var tempName = req.params.templateName.toString();
+	res.render(tempName,{});
 
 });
 
